e0b19fc8cbf13aa79c88a86deae0873acc25949e
Joao Vitor Souza <viictorsouza97@gmail.com>
2022-12-22 17:52:32 -0300

    feat: add Input component

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,5 +1,12 @@
+import Input from 'components/Input';
 import { PageHeader } from 'components/PageHeader';
 
 export function EditContact() {
-  return (<PageHeader title="Editar Contato" />);
+  return (
+    <>
+      <PageHeader title="Editar Contato" />
+      <Input type="text" placeholder="Nome" />
+
+    </>
+  );
 }
