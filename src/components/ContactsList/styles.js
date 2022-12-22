61a43ac7948e9a3e4e72d6bb623102c7f31a2190
Joao Vitor Souza <viictorsouza97@gmail.com>
2022-12-22 15:56:45 -0300

    feat: add contacts list

--- src/components/ContactsList/styles.js
+++ src/components/ContactsList/styles.js
@@ -29,3 +29,73 @@ export const Header = styled.header`
     }
   }
 `;
+
+export const ListContainer = styled.div`
+  margin-top: 24px;
+
+  header {
+    margin-bottom: 6px;
+
+    button {
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+
+      span {
+        margin-right: 8px;
+        font-weight: bold;
+        color: ${({ theme }) => theme.colors.primary.main};
+        font-size: 16px;
+      }
+    }
+  }
+`;
+
+export const Card = styled.div`
+  background: #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  padding: 16px;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & + & {
+    margin-top: 16px;
+  }
+
+  .info {
+    .contact-name {
+      display: flex;
+      align-items: center;
+
+      small {
+        margin-left: 8px;
+        background: ${({ theme }) => theme.colors.primary.lighter};
+        color: ${({ theme }) => theme.colors.primary.main};
+        font-weight: bold;
+        padding: 4px;
+        border-radius: 4px;
+        text-transform: uppercase;
+      }
+    }
+
+    span {
+      display: block;
+      color: ${({ theme }) => theme.colors.gray[200]};
+      font-size: 14px;
+    }
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+
+    button {
+      background: transparent;
+      border: none;
+      margin-left: 8px;
+    }
+  }
+`;
