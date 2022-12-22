bb59a9554730ee9e7aee54b891bdbc29a0c22e6d
Joao Vitor Souza <viictorsouza97@gmail.com>
2022-12-22 17:53:52 -0300

    feat: add Select input component

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,12 +1,16 @@
 import Input from 'components/Input';
 import { PageHeader } from 'components/PageHeader';
+import Select from 'components/Select';
 
 export function EditContact() {
   return (
     <>
       <PageHeader title="Editar Contato" />
       <Input type="text" placeholder="Nome" />
-
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
     </>
   );
 }
