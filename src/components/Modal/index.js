960a7f54dcc2c85b53da6e898cdf3d42e91a858a
Joao Vitor Souza <viictorsouza97@gmail.com>
2022-12-23 16:05:58 -0300

    feat: add react portals in Modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,20 +1,24 @@
 import Button from 'components/Button';
 import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
 import * as S from './styles';
 
-export const Modal = ({ danger }) => (
-  <S.Overlay>
-    <S.Container danger={danger}>
-      <h1>Titulo do Modal</h1>
-      <p>Esta ação não poderá ser desfeita!</p>
+export const Modal = ({ danger }) => {
+  return ReactDOM.createPortal(
+    <S.Overlay>
+      <S.Container danger={danger}>
+        <h1>Titulo do Modal</h1>
+        <p>Esta ação não poderá ser desfeita!</p>
 
-      <footer>
-        <button type="button" className="cancel-button">Cancelar</button>
-        <Button type="button" danger={danger}>Deletar</Button>
-      </footer>
-    </S.Container>
-  </S.Overlay>
-);
+        <footer>
+          <button type="button" className="cancel-button">Cancelar</button>
+          <Button type="button" danger={danger}>Deletar</Button>
+        </footer>
+      </S.Container>
+    </S.Overlay>,
+    document.getElementById('modal-root'),
+  );
+};
 
 Modal.propTypes = {
   danger: PropTypes.bool,
